3392e7eb3f2b35c7392a93e4572c48406f88be81
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-10 00:48:10 +0300

    Rename calc_size to get_size

--- src/image.c
+++ src/image.c
@@ -1,12 +1,12 @@
 #include <malloc.h>
 #include "image.h"
 
-static inline size_t calc_size(struct dimensions dims) {
+static inline size_t get_size(struct dimensions dims) {
   return dims.width * dims.height * sizeof(struct pixel);
 }
 
 struct image image_create(struct dimensions dims) {
-  return (struct image) { dims, malloc(calc_size(dims)) };
+  return (struct image) { dims, malloc(get_size(dims)) };
 }
 
 void image_destroy(struct image *img) { free(img->data); }
