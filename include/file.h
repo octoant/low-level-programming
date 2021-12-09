8b7d86acfaeb1618f64e9d451930b9d9dfb93ec1
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-10 01:16:34 +0300

    Update file.*

--- include/file.h
+++ include/file.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 enum open_status { OPEN_OK = 0, OPEN_ERROR };
-enum open_status file_open(FILE **, const char *, const char *);
-
 enum close_status { CLOSE_OK = 0, CLOSE_ERROR };
+
+enum open_status file_open(FILE **, const char *, const char *);
 enum close_status file_close(FILE **);
 
 #endif
--- src/file.c
+++ src/file.c
@@ -1,9 +1,18 @@
 #include "file.h"
 
-enum open_status file_open(FILE **file, const char *path, const char *mode) {
-  return OPEN_OK;
+enum open_status file_open(FILE **file, const char *pathname,
+                           const char *mode) {
+  if ((*file = fopen(pathname, mode)) == NULL) {
+    return OPEN_ERROR;
+  } else {
+    return OPEN_OK;
+  }
 }
 
 enum close_status file_close(FILE **file) {
-  return CLOSE_OK;
+  if (fclose(*file) == EOF) {
+    return CLOSE_ERROR;
+  } else {
+    return CLOSE_OK;
+  }
 }
