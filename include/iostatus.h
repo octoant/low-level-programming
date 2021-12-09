95ac1c1e5615790f2329aea48504244b1065ef70
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-10 00:30:25 +0300

    Update io-status

--- include/iostatus.h
+++ include/iostatus.h
@@ -1,6 +1,13 @@
 #ifndef IO_STATUS_H
 #define IO_STATUS_H
 
+enum read_status {
+  READ_OK = 0,
+  READ_INVALID_SIGNATURE,
+  READ_INVALID_BITS,
+  READ_INVALID_HEADER
+};
 
+enum write_status { WRITE_OK = 0, WRITE_ERROR };
 
 #endif
