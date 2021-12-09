de572e210d1b5f3f743781e0bcc1d63d324afadc
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-10 00:24:07 +0300

    Ass file.h, iostatus.h

--- include/file.h
+++ include/file.h
@@ -1,12 +1,5 @@
 #ifndef FILE_H
 #define FILE_H
 
-enum read_status {
-  READ_OK = 0
-};
-
-enum write_status {
-  WRITE_OK = 0
-};
-
 #endif
+
---
+++ include/iostatus.h
@@ -0,0 +1,6 @@
+#ifndef IO_STATUS_H
+#define IO_STATUS_H
+
+
+
+#endif
