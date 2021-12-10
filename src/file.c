52a5d682c983d5ca41419a9eb993ddddb713bc47
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-10 05:00:54 +0300

    Update

--- src/file.c
+++ src/file.c
@@ -2,17 +2,15 @@
 
 enum open_status file_open(FILE **file, const char *pathname,
                            const char *mode) {
-  if ((*file = fopen(pathname, mode)) == NULL) {
+  if ((*file = fopen(pathname, mode)) == NULL)
     return OPEN_ERROR;
-  } else {
+  else
     return OPEN_OK;
-  }
 }
 
 enum close_status file_close(FILE **file) {
-  if (fclose(*file) == EOF) {
+  if (fclose(*file) == EOF)
     return CLOSE_ERROR;
-  } else {
+  else
     return CLOSE_OK;
-  }
 }
