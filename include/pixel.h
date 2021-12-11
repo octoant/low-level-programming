47da20daf61de20e58143fd106bb700829f7eee2
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-11 18:40:19 +0300

    Update README.md

--- include/pixel.h
+++
@@ -1,8 +0,0 @@
-#ifndef PIXEL_H
-#define PIXEL_H
-
-#include <stdint.h>
-
-struct pixel { uint8_t r, g, b; };
-
-#endif
--- include/transform.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSFORM_H
-#define TRANSFORM_H
-
-#include "image.h"
-
-struct image rotate_left(struct image);
-
-#endif
