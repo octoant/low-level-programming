0944e676a1ca504c38dbea1f4330ad005bfa511c
Bobur Zakirov <bobur.octo@gmail.com>
2022-01-06 23:09:36 +0300

    Complete the last assignment

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
+#include <sys/resource.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #include "bmp.h"
 #include "file.h"
 #include "image.h"
-#include "transform.h"
+#include "sepia.h"
 #include "utils.h"
+
+#define OUT stderr
 
 #define EXECUTABLE "image-transformer"
 
@@ -18,46 +22,76 @@ static const char *w_msg[] = {
 };
 
 void usage() {
-  fprintf(stderr,
-          "Usage: ./" EXECUTABLE " <source-image> <transformed-image>\n");
+  fprintf(OUT, "Usage: ./" EXECUTABLE " <source-image>\n");
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 3) usage();
-  if (argc < 3) err("Not enough arguments\n");
-  if (argc > 3) err("Too many arguments\n");
+static void test_avg_time(struct image, int, FILE *);
 
-  FILE *in, *out;
-  if (file_open(&in, argv[1], "rb") != 0) {
+int main(int argc, char *argv[]) {
+  if (argc != 2) usage();
+  if (argc < 2) err("Not enough arguments\n");
+  if (argc > 2) err("Too many arguments\n");
+
+  FILE *in, *out_c, *out_asm;
+  if (file_open(&in, argv[1], "rb") != 0 ||
+      file_open(&out_c, "sepia_out_c.bmp", "wb") != 0 ||
+      file_open(&out_asm, "sepia_out_asm.bmp", "wb") != 0) {
     err("Could not open the file for reading\n");
   }
-  if (file_open(&out, argv[2], "wb") != 0) {
-    file_close(&in);
-    err("Could not create a file for writing\n");
-  }
-
   struct image source = {0};
 
   enum read_status rstat = from_bmp(in, &source);
   file_close(&in);
   if (rstat != READ_OK) {
-    fprintf(stderr, "%s\n", r_msg[rstat]);
+    fprintf(OUT, "%s\n", r_msg[rstat]);
     return rstat;
   }
 
-  struct image transformed = rotate_left(source);
+  struct image img_c = sepia_c_inplace(source);
+  struct image img_asm = sepia_sse(source);
 
-  enum write_status wstat = to_bmp(out, &transformed);
-  file_close(&out);
-  if (wstat != WRITE_OK) {
-    image_destroy(&source);
-    image_destroy(&transformed);
-    fprintf(stderr, "%s\n", w_msg[wstat]);
-    return wstat;
+  enum write_status wstat_c = to_bmp(out_c, &img_c);
+  file_close(&out_c);
+  if (wstat_c != WRITE_OK) {
+    image_destroy(&img_c);
+    fprintf(OUT, "%s\n", w_msg[wstat_c]);
+  }
+  enum write_status wstat_asm = to_bmp(out_asm, &img_asm);
+  file_close(&out_asm);
+  if (wstat_asm != WRITE_OK) {
+    image_destroy(&img_asm);
+    fprintf(OUT, "%s\n", w_msg[wstat_asm]);
   }
 
+  fprintf(OUT, "Image size: %" PRId32 "x%" PRId32 "\n", source.dims.width, source.dims.height);
+
+  test_avg_time(source, 64, OUT);
+
   image_destroy(&source);
-  image_destroy(&transformed);
+  image_destroy(&img_c);
+  image_destroy(&img_asm);
 
   return 0;
+}
+
+static void test_avg_time(struct image img, int n, FILE *file) {
+  struct rusage r;
+  struct timeval start, middle, end;
+  long total_c = 0, total_asm = 0;
+  for (int i = 0; i < n; i++) {
+    getrusage(RUSAGE_SELF, &r);
+    start = r.ru_utime;
+    struct image img_c = sepia_c_inplace(img);
+    getrusage(RUSAGE_SELF, &r);
+    middle = r.ru_utime;
+    struct image img_asm = sepia_sse(img);
+    getrusage(RUSAGE_SELF, &r);
+    end = r.ru_utime;
+    total_c += ((middle.tv_sec - start.tv_sec) * 1000000L) + middle.tv_usec - start.tv_usec;
+    total_asm += ((end.tv_sec - middle.tv_sec) * 1000000L) + end.tv_usec - middle.tv_usec;
+    image_destroy(&img_c);
+    image_destroy(&img_asm);
+  }
+  fprintf(file, "Avg time elapsed in microseconds for c: %ld\n", total_c / n);
+  fprintf(file, "Avg time elapsed in microseconds for asm: %ld\n", total_asm / n);
 }
